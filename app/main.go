9f7e4f962444e954b15ee95e4a1fbbc21629135c
Anton <antonderegt@pm.me>
2020-08-14 12:24:16 +0200

    Add sample endpoints

--- app/main.go
+++ app/main.go
@@ -1,13 +1,26 @@
 package main
 
-import "github.com/gofiber/fiber"
+import (
+	"github.com/antonderegt/go-postcodes/app/book"
+	"github.com/gofiber/fiber"
+)
+
+func helloWorld(c *fiber.Ctx) {
+	c.Send("Hello, World!")
+}
+
+func setupRoutes(app *fiber.App) {
+	app.Get("/", helloWorld)
+
+	app.Get("/api/v1/book", book.GetBooks)
+	app.Get("/api/v1/book/:id", book.GetBook)
+	app.Post("/api/v1/book", book.NewBook)
+	app.Delete("/api/v1/book/:id", book.DeleteBook)
+}
 
 func main() {
 	app := fiber.New()
 
-	app.Get("/", func(c *fiber.Ctx) {
-		c.Send("Hello Jeroen!")
-	})
-
+	setupRoutes(app)
 	app.Listen(3000)
 }
